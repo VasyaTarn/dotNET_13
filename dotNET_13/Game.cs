68ece34ce8a07936645442be68b6d6483fd74266
vasat <vasat@VASAT>
2023-05-07 22:40:42 +0300

    Add project files.

---
+++ dotNET_13/Game.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNET_13
+{
+    class Game
+    {
+        private string name;
+        private string rules;
+        private string author;
+        public Game(string name, string rules, string author)
+        {
+            this.name = name;
+            this.rules = rules;
+            this.author = author;
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Rules
+        {
+            get
+            {
+                return rules;
+            }
+            set
+            {
+                rules = value;
+            }
+        }
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                author = value;
+            }
+        }
+        public void NewGame(Game game)
+        {
+            Console.WriteLine($"New game {game.Name}");
+        }
+        public void DownloadGame(Game game)
+        {
+            Console.WriteLine($"Downloading game {game.Name}");
+        }
+        public void ShowRules(Game game)
+        {
+            Console.WriteLine($"Rules:\n{game.Rules}");
+        }
+        public void ShowAuthor(Game game)
+        {
+            Console.WriteLine($"The author is {game.Author}");
+        }
+        public void Exit(Game game)
+        {
+            Console.WriteLine($"The game {game.Name} closed");
+        }
+    }
+}
---
+++ dotNET_13/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNET_13
+{
+    class Program
+    {
+        delegate void GameMethod(Game game);
+        static void Main(string[] args)
+        {
+            Game g = new Game("GameName1", "GameRules1", "GameAuthor1");
+            GameMethod[] gm1 = new GameMethod[5] { new GameMethod(g.Exit), new GameMethod(g.NewGame), new GameMethod(g.DownloadGame), new GameMethod(g.ShowRules), new GameMethod(g.ShowAuthor) };
+            int answ = 1;
+            while (answ != 0)
+            {
+                Console.WriteLine("1 - новая игра\n2 - загрузить игру\n3 - правила\n4 - об авторе\n0 - выход");
+                answ = Convert.ToInt32(Console.ReadLine());
+                if (answ >= 0 && answ < 5)
+                {
+                    gm1[answ](g);
+                }
+                else
+                    Console.WriteLine("Wrong input");
+            }
+        }
+    }
+}
+
